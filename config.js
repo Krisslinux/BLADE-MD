78ab06b6e58755bbb31c306ec69cdaf1fb67164d
Krisslinux <123703291+Krisslinux@users.noreply.github.com>
2024-08-06 09:20:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 //═══════[Required Variables]════════\\
 global.owner = process.env.OWNER_NUMBER || '2349160878368'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://ahil1:787191784abhi@ahil1.kzr1tt3.mongodb.net/?retrywrites=true&w=majority"
-global.port= process.env.PORT || 5000
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Kriss:Boluwatife99@cluster0.gnwpzxk.mongodb.net/?retryWrites=true&w=majority"
 global.email = 'maxdavid126@gmail.com'
 global.github = 'https://github.com/Bladeh4x/BLADE-MD'
 global.location = 'Lahore pakistan'
@@ -15,9 +14,9 @@ global.devs = '2349160878368'
 global.website = 'https://github.com/Bladeh4x/BLADE-MD' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3f3ae4aace2d012c11509.jpg'
 module.exports = {
-  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
-  author:  process.env.PACK_AUTHER ||  'BLADE BOT',
-  packname:  process.env.PACK_NAME || 'MADE BY ALEX DAVID',
+  sessionName: process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0ZXLzZ4NGc4SzhWQXlHcjNySDhaUkpFeVhRaFhFODdibVNFNjZJVmVtVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoic3I4Ry9zU2ZqVTBObmV0b0Rld2dJeU5STkMzNW1UZ2FLQXNQem1rTnFBaz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJlUGRoeXpkRjRVV1VzR08yWlhiWlIyQUlGL3RVYUFhZENocEFzOXFZRUg4PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJjWDBrTHA4K01DOWJ4TnhPWitPS2UzdzE3SEhZVEhBM3U1K1E3Ui93MndzPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImdFWmdDMkZMMzNHdU9HTzMwc0ZDWWwwNVU5VXpORXVGYVJYc3JCSnArSFU9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IndhTkpXOW1MdkdYeXMxUmFDbXBscnpTZTQyZC92cU51YU12dyt3Z3FLR1E9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMEpJMmcxT0pmaVBoS0tURXVVdEdHSkhIdlhwdXl1VXpBWmFqalN4R2lYST0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicmdMWFdLeEF4Uk8yZkJ1VkZEbnU5cjR4VTRRR2VwTkt3d21mRlFJcDhrST0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InROK2Y0UTRKL1FZV2JHcGdMcGMyMEdUYUxQWkhYY2g2Zm5EZXphNlVWdWZFckJrNWtnc1AwQ2lmeDdZK0JRWElPL3RlMHJ5enh1S1RZaDdibnBwaWdRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTY4LCJhZHZTZWNyZXRLZXkiOiJmdDZPWEI5Ym04OG1BU3pyWmNtZnVoSGYvNHRVOHZLUHhIekxKRlV2Mis4PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiIwN1FGaG4wZlFSSzFvTV9NUHlzVWFBIiwicGhvbmVJZCI6IjVkMGIxMzlhLTQ2YTgtNDdmNy1hNzc5LWQ0Yzc1YjQzZjNiOCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ2eDBkWTVJSEJBMTZBNVJ2ZHVqVHYyL3h5QXM9In0sInJlZ2lzdGVyZWQiOmZhbHNlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImhnOFRzcDRwakdpd051VUg4anVlb0ZndGtDND0ifSwicmVnaXN0cmF0aW9uIjp7fSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ09XVnhsWVEyTGpIdFFZWUFTQUFLQUE9IiwiYWNjb3VudFNpZ25hdHVyZUtleSI6InI2VThCalZsQzJwbU55TE54Yy8xWTg0OW1mbXVUME1IS09BRUx2SnpQSHM9IiwiYWNjb3VudFNpZ25hdHVyZSI6IlZjdzBDUkJNUGFmTm4rZnZnY3RwNkk2RnovL0NkWGc1TXFuZFRuUW5pT1FhYjBTTXRReEtnajh6dGhtVkxVK1c0SEVKeEpuMFNWeFBmV0QzY3RKc0N3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiJ6VGp0MnZqbFhZM3FLbHVkbnhYYnRHL1hiQ3NteVl6UWF2Z0R4d1RUMTFhWTQ5VHN1REJKSURPMUk2cnJnbHhJZU9aYTl1TkNiR284K2NVcktTZlVpdz09In0sIm1lIjp7ImlkIjoiMjM0ODA1MjYyOTY1Mjo0OEBzLndoYXRzYXBwLm5ldCJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMzQ4MDUyNjI5NjUyOjQ4QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmErbFBBWTFaUXRxWmpjaXpjWFA5V1BPUFpuNXJrOURCeWpnQkM3eWN6eDcifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MjI5MzIzMTZ9",      //Put Your Session Id Here
+  author:  process.env.PACK_AUTHER ||  'KRISS BOT',
+  packname:  process.env.PACK_NAME || 'Daniel',
   
   botname:   process.env.BOT_NAME === undefined ? "BLADE-MD" : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'alexdavid' : process.env.OWNER_NAME,  
